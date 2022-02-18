c15b33fd430dbbad0bcd2abab8918ad207f25bb4
Badboy-xd <98704993+Badboy-xd@users.noreply.github.com>
2022-02-18 14:44:14 +0530

    Update music.py

--- Noinoi/PLUGINS/music.py
+++ Noinoi/PLUGINS/music.py
@@ -47,9 +47,9 @@ async def play(c: Client, m: Message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("• Gʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"),
+                InlineKeyboardButton("• Gʀᴏᴜᴘ", url=f"https://t.me/{EnglishHindi_Chatting_Group}"),
                 InlineKeyboardButton(
-                    "• Cʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"),
+                    "• Cʜᴀɴɴᴇʟ", url=f"https://t.me/{ganda_pipul}"),
             ]
         ]
     )
